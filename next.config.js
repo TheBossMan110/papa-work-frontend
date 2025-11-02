7e4f4d3c399be30fd4ed023678e6027fc9710edf
ProgrammerDammer <programming984@gmail.com>
2025-11-02 20:47:08 +0500

    error

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["work-papa.onrender.com"],
+  },
+};
+
+module.exports = nextConfig;
